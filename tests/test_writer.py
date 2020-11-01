4170e7864a6fb3773fa5816a12ebfb2b2d0fd37c
ahaeberle <andreas.haeberle@vipco.de>
2020-11-01 11:49:19 +0100

    some renaming of global variable to g_output and the writer function to write()

--- tests/test_writer.py
+++ tests/test_writer.py
@@ -3,16 +3,16 @@ import sys
 
 import mdpre # TODO: fix this later 
 
-def test_stdout_writer():
-    writer_stdout = mdpre.create_output_file()
-    writer_stdout.writeMarkdown('| sys.stdout works\n')
-    mdpre.exit_script(writer_stdout, 0)
+def test_stdout_g_output():
+    g_output_stdout = mdpre.create_output_file()
+    g_output_stdout.writeMarkdown('| sys.stdout works\n')
+    mdpre.exit_script(g_output_stdout, 0)
     
-def test_file_writer():
-    writer_file   = mdpre.create_output_file('test.md')
-    writer_file.writeMarkdown('| file output works\n')
-    mdpre.exit_script(writer_file, 0)
+def test_file_g_output():
+    g_output_file   = mdpre.create_output_file('test.md')
+    g_output_file.writeMarkdown('| file output works\n')
+    mdpre.exit_script(g_output_file, 0)
 
 if __name__ == "__main__":
-    test_stdout_writer()
-    test_file_writer()
+    test_stdout_g_output()
+    test_file_g_output()
